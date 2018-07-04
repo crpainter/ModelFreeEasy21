86f594de1b997b31f54c75741ffb4a269fdda787
Chris <cpainter@wharton.upenn.edu>
2018-07-04 23:27:49 +0200

    Made small fixes to environment implementation and moved on to agent design

--- environment.py
+++ environment.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def draw(num):
-    roll = np.random.rand(1);
     color = -1;
+    roll = np.random.rand(1);
     if (roll < (2 / 3)):
         color = 1
     num = num + (color * np.random.randint(1, 10));
@@ -11,21 +11,29 @@ def draw(num):
 
 def environment(s, a):
     sp = s;
-    if (s == []):
+    r = 0;
+    if (sp == []):
+        sp = np.zeros(2);
         sp[0] = np.random.randint(1,10);
         sp[1] = np.random.randint(1, 10);
-        r = 0;
+        return sp, r;
     if (a == 'hit'):
         sp[0] = draw(s[0])
         if (sp[0] < 1 or sp[0] > 21):
             r = -1
+            return sp, r;
     if (a == 'stick'):
         while(sp[1] < 17):
-            sp[1] = draw([sp[1]]);
+            sp[1] = draw(sp[1]);
+        if (sp[1] > 21):
+            r = 1;
+            return sp, r
         if(sp[1] > sp[0]):
             r = -1;
-        if (sp[1] > sp[0]):
-            r = -1;
-
+        if (sp[1] < sp[0]):
+            r = 1;
     return sp, r
+
+
+
 
